4789ef9f31faea4f7e6dcda1ccff8762086689c6
mustafiya <mustafiyasheikh14@gmail.com>
2025-10-06 09:01:06 +0530

    build support page component

--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -2,52 +2,56 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="container-fluid">
-      <div className="row support">
-        <div className="col-2"></div>
-        <div className="col-4">
-          <h3 className="mb-5 fs-5">Support Portal</h3>
-          <h4 className="fs-5 text-small">
+    <div className="container-fluid" id="supportHero">
+      <div className=" p-3 mt-5" id="supportWrapper">
+        <h4 className="ml-5">Support Portal</h4>
+        <a href="" style={{ color: "white" }}>
+          Track Tickets
+        </a>
+      </div>
+
+      <div className=" row mb-5 mx-5">
+        <div className=" col p-5 mb-5">
+          <h2 className="fs-3">
             Search for an answer or browse help topics to create a ticket
-          </h4>
+          </h2>
           <input
-            className="supportInput"
-            placeholder="Eg: how do i activate F&O, why is my order getting rejected....."
-          ></input>
-          <br />
-          <a href="" className="" style={{ color: "white" }}>
+            type="text"
+            placeholder="Eg: how do i activate F&O , why is my orger getting rejected"
+          />
+          <a href="" style={{ color: "white" }}>
             Track account opening
           </a>{" "}
-          &nbsp; &nbsp;
+          &nbsp;
           <a href="" style={{ color: "white" }}>
             Track segment activation
           </a>{" "}
-          &nbsp; &nbsp;
+          &nbsp;
           <a href="" style={{ color: "white" }}>
-            Intraday margins
+            Intraday.margins
           </a>{" "}
-          &nbsp; &nbsp;
+          &nbsp;
           <a href="" style={{ color: "white" }}>
             Kite user manual
           </a>
         </div>
-
-        <div className="col-1"></div>
-
-        <div className="col-3">
-          <h3 className="text-center mb-2">
-            <a href="">Track Tickets</a>
-          </h3>
-          <h4>Featured</h4>
+        <div className=" col p-5 mb-5">
+          <h2 className="fs-3">Featured</h2>
           <p>
-            1.<a href="">Current Takeovers and Delisting - January 2024</a>
+            1.
+            <a href="" style={{ color: "white" }}>
+              Current Takeovers and Delisting - January 2024
+            </a>
           </p>
           <p>
-            2.<a href="">Latest Intraday leverages - MIS & CO</a>
+            2.
+            <a href="" style={{ color: "white" }}>
+              Latest Intraday Leverages - MIS & CO
+            </a>
           </p>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
